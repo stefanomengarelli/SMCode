d6a606b813308aed48b157ae38494fe76c8bb827
Stefano Mengarelli <stefanomengarelli@outlook.it>
2024-06-26 18:41:50 +0200

    Workaround.

--- SMCode/Users/SMUser.cs
+++ SMCode/Users/SMUser.cs
@@ -1,7 +1,7 @@
 /*  ===========================================================================
  *  
  *  File:       SMUser.cs
- *  Version:    2.0.0
+ *  Version:    2.0.30
  *  Date:       May 2024
  *  Author:     Stefano Mengarelli  
  *  E-mail:     info@stefanomengarelli.it
