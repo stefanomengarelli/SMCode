c6fccbf46545253a2093cafe0b83778385629093
Stefano Mengarelli <stefanomengarelli@outlook.it>
2025-03-17 15:48:52 +0100

    Empty.cs unit full comments.

--- SMCode/Core/Empty.cs
+++ SMCode/Core/Empty.cs
@@ -1,12 +1,12 @@
 /*  ===========================================================================
  *  
  *  File:       Empty.cs
- *  Version:    2.0.0
- *  Date:       April 2024
+ *  Version:    2.0.221
+ *  Date:       March 2025
  *  Author:     Stefano Mengarelli  
  *  E-mail:     info@stefanomengarelli.it
  *  
- *  Copyright (C) 2010-2024 by Stefano Mengarelli - All rights reserved - Use, 
+ *  Copyright (C) 2010-2025 by Stefano Mengarelli - All rights reserved - Use, 
  *  permission and restrictions under license.
  *
  *  SMCode application class: empty.
@@ -35,13 +35,17 @@ namespace SMCodeSystem
          */
 
         /// <summary>Return true if date is null or empty (minimum value).</summary>
+        /// <param name="_Value">The date value to check.</param>
+        /// <returns>True if the date is null or the minimum value, otherwise false.</returns>
         public bool Empty(DateTime _Value)
         {
             if (_Value == null) return true;
             else return _Value <= DateTime.MinValue;
         }
 
-        /// <summary>Ritorna true se la stringa passata è nulla, vuota o composta da soli spazi.</summary>
+        /// <summary>Ritorna true se la data passata è nulla o vuota (valore minimo).</summary>
+        /// <param name="_Value">Il valore della data da controllare.</param>
+        /// <returns>True se la data è nulla o il valore minimo, altrimenti false.</returns>
         public bool Empty(DateTime? _Value)
         {
             if (_Value.HasValue) return _Value.Value <= DateTime.MinValue;
@@ -49,6 +53,9 @@ namespace SMCodeSystem
         }
 
         /// <summary>Returns true if object is null.</summary>
+        /// <param name="_Value">The object to check.</param>
+        /// <param name="_TestIfStringIsEmpty">Whether to test if the string is empty.</param>
+        /// <returns>True if the object is null, otherwise false.</returns>
         public bool Empty(object _Value, bool _TestIfStringIsEmpty = false)
         {
             if (_Value == null) return true;
@@ -58,6 +65,8 @@ namespace SMCodeSystem
         }
 
         /// <summary>Returns true if string is null, empty or contains only spaces.</summary>
+        /// <param name="_Value">The string to check.</param>
+        /// <returns>True if the string is null, empty or contains only spaces, otherwise false.</returns>
         public bool Empty(string _Value)
         {
             if (_Value == null) return true;
@@ -65,6 +74,8 @@ namespace SMCodeSystem
         }
 
         /// <summary>Returns true if string array is null or with no items.</summary>
+        /// <param name="_Value">The string array to check.</param>
+        /// <returns>True if the string array is null or has no items, otherwise false.</returns>
         public bool Empty(string[] _Value)
         {
             if (_Value == null) return true;
