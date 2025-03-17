1eefd51b8c0fbb043ca4b28b87c6e6ee9e68ad92
Stefano Mengarelli <stefanomengarelli@outlook.it>
2025-03-17 15:09:47 +0100

    Cache.cs unit full comments.

--- SMCode/Core/Cache.cs
+++ SMCode/Core/Cache.cs
@@ -1,12 +1,12 @@
 /*  ===========================================================================
  *  
  *  File:       Cache.cs
- *  Version:    2.0.21
- *  Date:       May 2024
+ *  Version:    2.0.221
+ *  Date:       March 2025
  *  Author:     Stefano Mengarelli  
  *  E-mail:     info@stefanomengarelli.it
  *  
- *  Copyright (C) 2010-2024 by Stefano Mengarelli - All rights reserved - Use, 
+ *  Copyright (C) 2010-2025 by Stefano Mengarelli - All rights reserved - Use, 
  *  permission and restrictions under license.
  *
  *  SMCode cache values management class.
@@ -34,9 +34,15 @@ namespace SMCodeSystem
          *  ===================================================================
          */
 
-        /// <summary>Return cached value of parameter with id in to section. 
+        /// <summary>
+        /// Return cached value of parameter with id in to section. 
         /// If expire is greater than zero function will return empty string
-        /// if value is older than these days.</summary>
+        /// if value is older than these days.
+        /// </summary>
+        /// <param name="_Section">The section where the parameter is stored.</param>
+        /// <param name="_ParameterId">The ID of the parameter to read.</param>
+        /// <param name="_ExpirationDays">The number of days after which the cached value expires.</param>
+        /// <returns>Returns the cached value as a string, or an empty string if the value is expired or not found.</returns>
         public string CacheRead(string _Section, string _ParameterId, int _ExpirationDays)
         {
             string r = @"";
@@ -54,7 +60,13 @@ namespace SMCodeSystem
             return r;
         }
 
-        /// <summary>Write cached value of parameter with id in to section.</summary>
+        /// <summary>
+        /// Write cached value of parameter with id in to section.
+        /// </summary>
+        /// <param name="_Section">The section where the parameter will be stored.</param>
+        /// <param name="_ParameterId">The ID of the parameter to write.</param>
+        /// <param name="_Value">The value to be cached.</param>
+        /// <returns>Returns true if the value was successfully written, otherwise false.</returns>
         public bool CacheWrite(string _Section, string _ParameterId, string _Value)
         {
             bool r = false;
